90769161de5e52b34d149a5d1351bcdb2aa51140
blogscot <iain.diamond@gmail.com>
2016-05-10 16:06:27 +0100

    Fix failing unit tests

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,46 +1,29 @@
-from string import lowercase, maketrans
+from string import lowercase, maketrans, punctuation
 
 alphabet = lowercase
-tebahpla = alphabet[::-1]
+tebahpla = lowercase[::-1]
 CHUNK = 5
 
 
+def filter_punctuation(sentence):
+    return ''.join(filter(lambda s: s not in punctuation, sentence))
+
+
 def chunk(sentence, size=CHUNK):
-    """
-    Breaks a string into chunks.
-
-    :param sentence:        sentence to chunk
-    :param size:            chunk size
-    :return:                string of space separated chunks
-    """
-
     if len(sentence) <= size:
         return sentence
 
-    return ' '. join([''.join(list(item))
-                      for item in zip(*[iter(sentence)]*size)])
+    return sentence[:size] + " " + chunk(sentence[size:])
 
 
 def encode(seq):
-    """
-    Encodes a string into 5 character blocks
-
-    :param seq:     sentence to encode
-    :return:        Atbash encoded sentence
-    """
-
-    return chunk(seq
-                 .lower()
-                 .replace(" ", "")
-                 .translate(maketrans(alphabet, tebahpla)))
+    seq = seq.lower().replace(" ", "")
+    seq = filter_punctuation(seq).translate(maketrans(alphabet, tebahpla))
+    return chunk(seq)
 
 
 def decode(seq):
-    """
-    Decodes an Atbash encoded string
+    return (seq
+            .replace(" ", "")
+            .translate(maketrans(tebahpla, alphabet)))
 
-    :param seq:     Atbash encoded sentence
-    :return:        decoded sentence
-    """
-
-    return seq.translate(maketrans(tebahpla, alphabet))
