2a985274a28899a2c8c92b37f727e0f464d5e308
blogscot <iain.diamond@gmail.com>
2016-05-14 17:10:12 +0100

    Refactor Kindergarten solution

--- kindergarten-garden/garden.py
+++ kindergarten-garden/garden.py
@@ -1,21 +1,14 @@
 class Garden(object):
 
     _names = "Alice Bob Charlie David Eve Fred Ginny Harriet Ileana Joseph Kincaid Larry".split()
-
-    _plant_values = "Clover Grass Radishes Violets".split()
-    _plant_keys = "C G R V".split()
-    _plants = dict(zip(_plant_keys, _plant_values))
+    _plants = dict((p[0], p) for p in "Clover Grass Radishes Violets".split())
 
     def __init__(self, plants, students=_names):
-        self._children = dict(zip(sorted(students), range(len(students))))
+        self._students = {v: k * 2 for k, v in enumerate(sorted(students))}
         p = plants.split('\n')
-        self._row1 = p[0]
-        self._row2 = p[1]
+        self._row1, self._row2 = p[0], p[1]
 
-    def plants(self, child):
-        first_plant = self._children[child] * 2
-        child_plants = [self._row1[first_plant], self._row1[first_plant+1],
-                        self._row2[first_plant], self._row2[first_plant + 1]]
-
+    def plants(self, student):
+        pos = self._students[student]
+        child_plants = list(self._row1[pos:pos+2] + self._row2[pos:pos+2])
         return map(lambda x: self._plants[x], child_plants)
-
