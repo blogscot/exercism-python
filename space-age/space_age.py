89a5c503b71d87f0b4e5ea70c3d3f754a136d73f
blogscot <iain.diamond@gmail.com>
2016-05-16 15:04:33 +0100

    Refactor Space Age solution

--- space-age/space_age.py
+++ space-age/space_age.py
@@ -1,43 +1,20 @@
-EARTH = 31557600
+EARTH = 31557600.0
 
-years = {'Earth': EARTH,
-         'Mercury': EARTH * 0.2408467,
-         'Venus': EARTH * 0.61519726,
-         'Mars': EARTH * 1.8808158,
-         'Jupiter': EARTH * 11.862615,
-         'Saturn': EARTH * 29.447498,
-         'Uranus': EARTH * 84.016846,
-         'Neptune': EARTH * 164.79132,
-         }
+planet_data = {'earth': EARTH,
+               'mercury': EARTH * 0.2408467,
+               'venus': EARTH * 0.61519726,
+               'mars': EARTH * 1.8808158,
+               'jupiter': EARTH * 11.862615,
+               'saturn': EARTH * 29.447498,
+               'uranus': EARTH * 84.016846,
+               'neptune': EARTH * 164.79132,
+               }
 
 
 class SpaceAge:
-    def years_on(self, planet):
-        return round(self.seconds / years[planet], 2)
 
     def __init__(self, seconds):
-        self.seconds = float(seconds)
-
-    def on_earth(self):
-        return self.years_on('Earth')
-
-    def on_mercury(self):
-        return self.years_on('Mercury')
-
-    def on_venus(self):
-        return self.years_on('Venus')
-
-    def on_mars(self):
-        return self.years_on('Mars')
-
-    def on_jupiter(self):
-        return self.years_on('Jupiter')
-
-    def on_saturn(self):
-        return self.years_on('Saturn')
-
-    def on_uranus(self):
-        return self.years_on('Uranus')
-
-    def on_neptune(self):
-        return self.years_on('Neptune')
+        self.seconds = seconds
+        for planet, period in planet_data.items():
+            setattr(self, "on_" + planet,
+                    lambda p=period: round(seconds / p, 2))
